e1ed70600f72dfb7c76f00be0dad05ad25d39b99
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-09-02 20:04:54 +0700

    create client

--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 
 import Section from 'elements/Section';
 import Header from 'parts/Header';
+import Clients from 'parts/Clients';
+
 import HeroImage from 'assets/images/hero-image.png';
-import Fade from 'react-reveal/Fade';
 
 export default function Homepage() {
     return (
@@ -48,6 +50,7 @@ export default function Homepage() {
                         </div>
                     </div>
                 </Section>
+                <Clients></Clients>
             </main>
         </div>
     );
